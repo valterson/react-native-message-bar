03430febc243b17e3b25180c4c4c5da4938117a3
vitorog <vitor.olivg@gmail.com>
2017-04-05 14:47:04 -0300

    Permitindo que a altura seja redimensionada

--- MessageBar.js
+++ MessageBar.js
@@ -369,7 +369,7 @@ class MessageBar extends Component {
     this._apllyAnimationTypeTransformation();
 
     return (
-      <Animated.View style={{ transform: this.animationTypeTransform, height: this.state.height, backgroundColor: this.state.backgroundColor, borderColor: this.state.strokeColor, borderBottomWidth: 1, position: 'absolute', top: this.state.viewTopOffset, bottom: this.state.viewBottomOffset, left: this.state.viewLeftOffset, right: this.state.viewRightOffset, paddingTop: this.state.viewTopInset, paddingBottom: this.state.viewBottomInset, paddingLeft: this.state.viewLeftInset, paddingRight: this.state.viewRightInset }}>
+      <Animated.View style={{ transform: this.animationTypeTransform, backgroundColor: this.state.backgroundColor, borderColor: this.state.strokeColor, borderBottomWidth: 1, position: 'absolute', top: this.state.viewTopOffset, bottom: this.state.viewBottomOffset, left: this.state.viewLeftOffset, right: this.state.viewRightOffset, paddingTop: this.state.viewTopInset, paddingBottom: this.state.viewBottomInset, paddingLeft: this.state.viewLeftInset, paddingRight: this.state.viewRightInset }}>
         <TouchableOpacity onPress={()=>{this._alertTapped()}}>
           <View style={{ flexDirection: 'row', alignItems: 'center', padding: 15 }} >
             { this.renderImage() }
